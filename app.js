6a469a61d44af67c20ab4ddbd905e7ce29c4453e
Dalton Carr <daltonlcarr@yahoo.com>
2017-07-28 14:58:53 -0700

    lots of html, animations /fonts and icons added

--- app.js
+++ app.js
@@ -12,3 +12,33 @@ function Projects (projectName, dateCreated, skills, fileLocation) {
   this.fileLocation = fileLocation;
   allProjects.push(this);
 }
+
+// Get the modal
+var modal = document.getElementById('myModal');
+// Get the button that opens the modal
+var btn = document.getElementById("one");
+var btn2 = document.getElementById("two");
+var btn3 = document.getElementById("three");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+btn2.onclick = function() {
+    modal.style.display = "block";
+}
+btn3.onclick = function() {
+    modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
