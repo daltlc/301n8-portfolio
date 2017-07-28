dd89a0e3d6323976bfb652f6bdc9328b342f4946
Dalton Carr <daltonlcarr@yahoo.com>
2017-07-28 14:53:12 -0700

    constructor added

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-'use strict'
+'use strict';
+
+
+//global variable to store new projects
+var allProjects = []
+
+//constructor to contain projects
+function Projects (projectName, dateCreated, skills, fileLocation) {
+  this.projectName = projectName;
+  this.dateCreated = dateCreated;
+  this.skills = skills;
+  this.fileLocation = fileLocation;
+  allProjects.push(this);
+}
