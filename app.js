1bb8be508dbd286e60937660712c1d518d96bca4
Dalton Carr <daltonlcarr@yahoo.com>
2017-07-28 15:00:14 -0700

    indentation

--- app.js
+++ app.js
@@ -24,21 +24,21 @@ var btn3 = document.getElementById("three");
 var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 btn2.onclick = function() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 btn3.onclick = function() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+  modal.style.display = "none";
 }
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
