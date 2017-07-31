cca6983c6782cfe8132d0fceee6ba80617d95035
Dalton Carr <daltonlcarr@yahoo.com>
2017-07-30 19:49:15 -0700

    colors changed/filler text

--- projects.js
+++ projects.js
@@ -1,24 +1,24 @@
 'use strict';
 var projectData = [
   {
-    projectName:     'RLS',
+    projectName:     'Demo1',
     skills:         'Javscript',
     publishedOn:    '07-27-2017',
-    body:        '<p>Hello</p>',
+    body:        '<p>Nunc neque neque, placerat sit amet feugiat in, accumsan in risus. Quisque egestas, nulla sit amet aliquet fringilla, lectus ex interdum dui, at rutrum velit turpis id nulla. Fusce tortor eros, pretium sed odio at, luctus porta nunc. Aliquam erat volutpat. Vestibulum viverra erat velit, vitae blandit odio euismod sed. Donec egestas scelerisque consectetur. Aliquam erat volutpat. Mauris feugiat, leo vitae vehicula tristique, mi dui porttitor magna, eu scelerisque dolor nisi commodo diam. Sed in lectus ex. Curabitur arcu elit, condimentum quis dui ac, sodales cursus dui. Sed ultricies quis ante non varius. Suspendisse faucibus augue non dolor semper, id gravida orci elementum. Donec condimentum a eros ut aliquet.</p>',
     category:     'javascript',
   },
   {
-    projectName:     'Bob',
+    projectName:     'Demo2',
     skills:         'Javscript',
     publishedOn:    '07-28-2017',
-    body:        '<p>Hello</p>',
+    body:        '<p>Nunc neque neque, placerat sit amet feugiat in, accumsan in risus. Quisque egestas, nulla sit amet aliquet fringilla, lectus ex interdum dui, at rutrum velit turpis id nulla. Fusce tortor eros, pretium sed odio at, luctus porta nunc. Aliquam erat volutpat. Vestibulum viverra erat velit, vitae blandit odio euismod sed. Donec egestas scelerisque consectetur. Aliquam erat volutpat. Mauris feugiat, leo vitae vehicula tristique, mi dui porttitor magna, eu scelerisque dolor nisi commodo diam. Sed in lectus ex. Curabitur arcu elit, condimentum quis dui ac, sodales cursus dui. Sed ultricies quis ante non varius. Suspendisse faucibus augue non dolor semper, id gravida orci elementum. Donec condimentum a eros ut aliquet.</p>',
     category:     'python',
   },
   {
-    projectName:     'Carl',
+    projectName:     'Demo3',
     skills:         'Javscript',
     publishedOn:    '07-29-2017',
-    body:        '<p>Hello</p>',
+    body:        '<p>Nunc neque neque, placerat sit amet feugiat in, accumsan in risus. Quisque egestas, nulla sit amet aliquet fringilla, lectus ex interdum dui, at rutrum velit turpis id nulla. Fusce tortor eros, pretium sed odio at, luctus porta nunc. Aliquam erat volutpat. Vestibulum viverra erat velit, vitae blandit odio euismod sed. Donec egestas scelerisque consectetur. Aliquam erat volutpat. Mauris feugiat, leo vitae vehicula tristique, mi dui porttitor magna, eu scelerisque dolor nisi commodo diam. Sed in lectus ex. Curabitur arcu elit, condimentum quis dui ac, sodales cursus dui. Sed ultricies quis ante non varius. Suspendisse faucibus augue non dolor semper, id gravida orci elementum. Donec condimentum a eros ut aliquet.</p>',
     category:     'javascript',
   },
 ];
