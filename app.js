4c06454f70c85f4a8e8af67cbec6b2425fb01d1e
Dalton Carr <daltonlcarr@yahoo.com>
2017-07-31 19:54:24 -0700

    nav work/welcome fade in/out/issues with fading in projects now

--- app.js
+++ app.js
@@ -43,58 +43,38 @@ allProjects.forEach(function(project) {
 
 
 //NAV------------------------------------------------
-$('#projects').hide();
-$('#aboutMe').hide();
-$('#contact').hide();
-//Project selection
-$('.comp-top').on('click', 'li:first', function() {
-  $('#aboutMe').fadeOut();
-  $('#projects').fadeIn();
-});
-//About me selection
-$('.comp-top').on('click', 'li:nth-child(2)', function() {
-  $('#projects').fadeOut();
-  $('#aboutMe').fadeIn();
+
+$( document ).ready(function() {
+  $('#projects').hide();
+  $('#aboutMe').hide();
+  $('#contact').hide();
+
+  //Project selection
+  $('.comp-top').on('click', 'li:first', function() {
+    $('#aboutMe').fadeOut();
+    $('#projects').fadeIn();
+  });
+  //About me selection
+  $('.comp-top').on('click', 'li:nth-child(2)', function() {
+    $('#projects').fadeOut();
+    $('#aboutMe').fadeIn();
+  });
+  //Project selection Mobile
+  $('.mobile-top').on('click', 'li:first', function() {
+    $('#aboutMe').fadeOut();
+    $('#projects').fadeIn();
+  });
+  //About me selection Mobile
+  $('.mobile-top').on('click', 'li:nth-child(2)', function() {
+    $('#projects').fadeOut();
+    $('#aboutMe').fadeIn();
+  });
 });
 
-//Project selection Mobile
-$('.mobile-top').on('click', 'li:first', function() {
-  $('#aboutMe').fadeOut();
-  $('#projects').fadeIn();
+
+/*Beginning fade in welcome*/
+$( document ).ready(function() {
+  $('.comp-top li').hide();
+  $('.comp-top h1').delay(1000).fadeIn().delay(1000).fadeOut();
+  $('.comp-top li').delay(2800).fadeIn();
 });
-//About me selection Mobile
-$('.mobile-top').on('click', 'li:nth-child(2)', function() {
-  $('#projects').fadeOut();
-  $('#aboutMe').fadeIn();
-});
-
-
-
-// Get the modal
-// var modal = document.getElementById('myModal');
-// // Get the button that opens the modal
-// var btn = document.getElementById("one");
-// var btn2 = document.getElementById("two");
-// var btn3 = document.getElementById("three");
-//
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-// // When the user clicks on the button, open the modal
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-// btn2.onclick = function() {
-//   modal.style.display = "block";
-// }
-// btn3.onclick = function() {
-//   modal.style.display = "block";
-// }
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
