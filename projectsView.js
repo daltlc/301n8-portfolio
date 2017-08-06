b632e2e17d574f7301e6b7c1831fa979a56b763e
Dalton Carr <daltonlcarr@yahoo.com>
2017-08-06 00:30:25 -0700

    jQuery terminal

--- projectsView.js
+++ projectsView.js
@@ -36,3 +36,25 @@ $(document).ready(function() {
   })
 .tiType('Dalton Carr')
 })
+
+jQuery(function($, undefined) {
+  $('#term_demo').terminal(function(command) {
+    if (command !== '') {
+      try {
+        var result = window.eval(command);
+        if (result !== undefined) {
+          this.echo(new String(result));
+        }
+      } catch(e) {
+        this.error(new String(e));
+      }
+    } else {
+      this.echo('');
+    }
+  }, {
+    greetings: 'Javascript Interpreter',
+    name: 'js_demo',
+    height: 200,
+    prompt: 'js> '
+  });
+});
