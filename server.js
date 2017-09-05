e247a8c7342c5946b4063af06c055dba051a4cc9
Dalton Carr <daltonlcarr@yahoo.com>
2017-09-05 16:11:08 -0700

    port fix

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 'use strict';
+
+const pg = require('pg');
+const fs = require('fs');
 const express = require('express');
-const app = express();
-app.use(express.static('./public'));
-const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 // const requestProxy = require('express-request-proxy');
-// const opn = require('opn'); //for opening in browser
-// const bodyParser = require('body-parser').urlencoded({extended: true});
-app.listen(3000, function(){
-  console.log('server up!');
-})
+const PORT = process.env.PORT || 3000;
+const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-// const client = new pg.Client(conString);
+const conString = ''; // TODO: Don't forget to set your own conString
+const client = new pg.Client(conString);
+client.connect();
+client.on('error', err => console.error(err));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('./public'));
