763d7215c797724f487815cf9d252fddd109bf77
Dalton Carr <daltonlcarr@yahoo.com>
2017-09-05 16:06:37 -0700

    port fix

--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 const express = require('express');
 const app = express();
 app.use(express.static('./public'));
+const PORT = process.env.PORT || 3000;
 // const requestProxy = require('express-request-proxy');
 // const opn = require('opn'); //for opening in browser
 // const bodyParser = require('body-parser').urlencoded({extended: true});
-app.listen(4000, function(){
+app.listen(3000, function(){
   console.log('server up!');
 })
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
