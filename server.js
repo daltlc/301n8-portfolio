ef461c526ddc0e467d178e6b9e09bedc8999ab3f
Dalton Carr <daltonlcarr@yahoo.com>
2017-09-05 19:07:25 -0700

    trying to fix heroku

--- server.js
+++ server.js
@@ -3,23 +3,38 @@ const app = express();
 const requestProxy = require('express-request-proxy');
 // const opn = require('opn'); //for opening in browser
 // const bodyParser = require('body-parser').urlencoded({extended: true});
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
-// function proxyGitHub(request, response) {
-//   console.log('Routing GitHub request for', request.params[0]);
-//   (requestProxy({
-//     url: `https://api.github.com/${request.params[0]}`,
-//     headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
-//   }))(request, response);
-// }
+function proxyGitHub(request, response) {
+  console.log('Routing GitHub request for', request.params[0]);
+  (requestProxy({
+    url: `https://api.github.com/${request.params[0]}`,
+    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+  }))(request, response);
+}
 
-app.use(express.static('/public'));
+app.use(express.static('public'));
 
-// app.get('/github/*', proxyGitHub);
+app.get('/github/*', proxyGitHub);
 
-// app.get('/*', function(there, backAgain) {
-//   backAgain.sendFile('index.html', {root: './public'});
-// });
+app.get('/*', function(there, backAgain) {
+  backAgain.sendFile('index.html', {root: './public'});
+});
+
+// opn(`http://localhost:${PORT}`);  ///automatically opens in default browser
+
+// // (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+// app.get('/new', function(req, res){
+//   res.redirect('new.html');
+// })
+
+// app.post('/articles', bodyParser, function(request, response) {
+//   // REVIEW: This route will receive a new article from the form page, new.html,
+//   // and log that form data to the console. We will wire this up soon to actually
+//   // write a record to our persistence layer!
+//   console.log(request.body);
+//   response.send('Record posted to server!!');
+// })
 
 app.listen(PORT, function() {
   // lets you know which port your server has started on
